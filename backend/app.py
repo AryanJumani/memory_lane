e038c3313acff9b5b4f64acf21d91b7b49fd9a22
AryanJumani <aryanjumani10@gmail.com>
2025-05-01 22:02:57 -0400

    for exporting p2

--- backend/app.py
+++ backend/app.py
@@ -377,4 +377,5 @@ def get_closest_landmark(lat, long):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
